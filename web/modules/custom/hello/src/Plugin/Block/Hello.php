85767d8f3c7490401701e51c9d194f576b58ba37
riaze <ria.thas20@gmail.com>
2019-11-06 16:52:30 +0100

    commit for hello modules

---
+++ web/modules/custom/hello/src/Plugin/Block/Hello.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\hello\Plugin\Block;
+use Drupal\Core\Block\BlockBase; 
+
+/**
+ * Provides a hello block. 
+ * @Block(
+ *  id = "hello_block",
+ *  admin_label = @Translation("Hello!")
+ * ) 
+ */
+class Hello extends BlockBase {
+    /**
+     * Implements Drupal\Core\Block\BlockBase::build().
+     */  
+    Public function build() {
+      
+    $date_formatter = \Drupal::service('date.formatter');
+    $time = \Drupal::service('datetime.time')->getCurrentTime();
+    $user_name = \Drupal::currentUser()->getDisplayName();
+    return [
+        '#markup' => $this->t('welcome %name. It os %time', 
+         [
+         '%name' => $user_name,  
+         '%time' => $date_formatter->format($time, 'custom', 'H:i: s\s'),]),
+         '#cache' => [
+            // 'key' => ['hello_block'],
+            // 'contexts' => ['user'],
+            'max-age' => '0' 
+         ],   
+    ];
+    }
+}
